6479dae1447faf88d571930120bac2b2fc0f4f80
김은혜 <lemonherb0323@naver.com>
2021-06-30 22:33:40 +0900

    register, log in/out

---
+++ crud/blog/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.urls.resolvers import URLPattern
+from blog import views
+
+urlpatterns=[
+    path('', views.main, name = 'main'),
+    path('write/', views.write, name = 'write'),
+    path('write/create/', views.create, name = 'create'),
+    path('edit/<str:id>', views.edit, name ='edit'),
+    path('update/<str:id>', views.update, name='update'),
+    path('delete/<str:id>', views.delete, name ='delete'),
+    path('comment/<str:id>', views.comment, name='comment'),
+    path('hashtag/', views.hashtagform, name='hashtag'),
+    path('<int:hashtag_id>/search/', views.search, name='search'),
+]
--- crud/crud/urls.py
+++ crud/crud/urls.py
@@ -14,18 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-import blog.views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', blog.views.main, name = 'main'),
-    path('write/', blog.views.write, name = 'write'),
-    path('write/create/', blog.views.create, name = 'create'),
-    path('edit/<str:id>', blog.views.edit, name ='edit'),
-    path('update/<str:id>', blog.views.update, name='update'),
-    path('delete/<str:id>', blog.views.delete, name ='delete'),
-    path('comment/<str:id>', blog.views.comment, name='comment'),
-    path('hashtag/', blog.views.hashtagform, name='hashtag'),
-    path('<int:hashtag_id>/search/', blog.views.search, name='search'),
+    path('', include('blog.urls')),
+    path('account/', include('account.urls')),
 ]
