825edfcfbb910234ff93e42c5956d06a2d615529
김은혜 <lemonherb0323@naver.com>
2021-07-15 11:06:23 +0900

    add customUser_age, address in register

--- crud/account/views.py
+++ crud/account/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib import auth
+from .forms import UserInformationForm
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
-        form=UserCreationForm(request.POST)
-        if form.is_valid():
-            user=form.save()
+        form1 = UserCreationForm(request.POST)
+        form2 = UserInformationForm(request.POST)
+        if form1.is_valid() and form2.is_valid():
+            user = form1.save()
+            user_information = form2.save(commit=False)
+            user_information.user = user
+            user_information.save()
             auth.login(request, user)
             return redirect('main')
         else:
-            return render(request, 'account/signup.html', {'form':form})
+            return render(request, 'account/signup.html', {'form1':form1, 'form2':form2})
         
     else:
-        form=UserCreationForm()
-        return render(request, 'account/signup.html', {'form':form})
+        form1 = UserCreationForm()
+        form2 = UserInformationForm()
+        return render(request, 'account/signup.html', {'form1':form1, 'form2':form2})
 
 def login(request):
     if request.method == 'POST':
